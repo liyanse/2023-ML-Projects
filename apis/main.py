9186176193de2d07f11c8e3896f6b170824e5710
Lians Wanjiku <91662935+liyanse@users.noreply.github.com>
2023-01-15 15:26:13 +0300

    fghj

--- apis/main.py
+++ apis/main.py
@@ -1,5 +1,5 @@
 from typing import List
-from uuid import uuid4
+from uuid import UUID
 from fastapi import FastAPI
 
 from models import Gender,User
@@ -9,14 +9,14 @@ app = FastAPI()
 
 ## The database
 db: List[User] = [
-    User(
-        id=uuid4(), 
+       User(
+        id=UUID("20ddb18d-2de8-4db7-b66b-767c8343318e"), 
         first_name="Lians",
         last_name= "Wanjiku",
         gender= Gender.female     
         ),
     User(
-        id=uuid4(), 
+        id=UUID("166e7941-9837-4502-94d0-21375263b534"), 
         first_name="Shem",
         last_name= "Githinji",
         gender= Gender.male   
@@ -30,3 +30,12 @@ async def root():
 @app.get("/api.v1/users")
 async def fetch_users():
     return db;
+
+##How to add a new user 
+
+@app.post("/api.v1/users")
+async def register_new_user(user: User):
+    db.append(user)
+    return {"id": user.id}
+
+##How to send our post request to our API
