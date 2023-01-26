27f26737fbf648a802424a58a8e9b4c56a839ef5
Lians Wanjiku <91662935+liyanse@users.noreply.github.com>
2023-01-26 21:26:23 +0300

    3

--- apis/main.py
+++ apis/main.py
@@ -1,76 +1,39 @@
-## FASTAPI  is a modern fast web framework for builfing APIs with Python 3.6+
-##based on standard Python type hints. It's built on top of Starlette for the 
-## web parts and Pydantic for the data parts. Some of its key features include:
-## 1. Fast Performance          2. Easy to use
-## 3. Fast to code              3. Short
-## 5. Fewer bugs                6. Standard
-
-
-## Path Parameters
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-#Create an Enum class¶
-#Import Enum and create a sub-class that inherits from str and from Enum.
-#By inheriting from str the API docs will be able to know that the values must be of type string and will be able to render correctly.
-#Then create class attributes with fixed values, which will be the available valid values:
-from enum import Enum
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
-app = FastAPI()
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-## Query Parameters
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-## Request Body
 from fastapi import FastAPI
 from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
+from typing import Optional
 
 app = FastAPI()
 
+myDb = []
 
-@app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
-    if q:
-        result.update({"q": q})
-    return result
+class Basket(BaseModel):
+    id: int
+    name: str
+    price: float
+    product1: str
+    product2: Optional[str] = None
+    
+    
+@app.get("/")
+def read_root():
+    return {"Hi": "Help us add products to our Database"}
+
+@app.get("/baskets")
+def get_products():
+    return myDb
+
+@app.get("/baskets/{basket_id}")
+def get_a_product(basket_id: int):
+    basket = basket_id -1
+    return myDb[basket]
+
+@app.post("/baskets")
+def add_product(basket: Basket):
+    myDb.append(basket.dict())
+    return myDb[-1]
+
+@app.delete("baskets/{basket_id}")
+def delete_basket(basket_id: int):
+    myDb.pop(basket_id-1)
+    return{"task": "delete successful"}
+    
