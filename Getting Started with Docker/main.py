fe32f62be6ae938d70bcee730044e34dcebaaec1
Lians Wanjiku <91662935+liyanse@users.noreply.github.com>
2023-02-28 17:29:09 +0300

    Update main.py

--- Getting Started with Docker/main.py
+++ Getting Started with Docker/main.py
@@ -1,42 +1,33 @@
-from flask import Flask, request, jsonify
-from tensorflow import keras
+#import libraries
+import pickle
 import numpy as np
-import base64
-import cv2
+from PIL import Image
+from tensorflow.keras.preprocessing.image import img_to_array
+from flask import Flask, request, render_template, jsonify
 
-# Load the Keras model
-model = keras.models.load_model('myModel.h5')
-
-# Create a Flask app
+# Create an instance of Flask
 app = Flask(__name__)
 
-# Define a route to handle image classification
+# Load pretrained TF model
+model = pickle.load(open('model.pickle', 'rb'))
+
+# Define the route for the prediction
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the image data from the request
-    image_data = request.json['image']
+    # Get the image passed in
+    image = request.files['image']
+    # Convert the image to an array
+    image_array = img_to_array(Image.open(image))
+    # Reshape the image according to model
+    image_array = image_array.reshape((1, image_array.shape[0], image_array.shape[1], image_array.shape[2]))
+    # Make the prediction
+    prediction = model.predict(image_array)
+    # Get the predicted class label
+    prediction_class = np.argmax(prediction)
+    # Return the results
+    return render_template('index.html', prediction_class=prediction_class,
+                           image=image)
 
-    # Convert the base64 image data to a numpy array
-    image_bytes = base64.b64decode(image_data)
-    image_array = np.frombuffer(image_bytes, dtype=np.uint8)
-    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-
-    # Preprocess the image
-    image = cv2.resize(image, (224, 224))
-    image = image.astype('float32') / 255.0
-    image = np.expand_dims(image, axis=0)
-
-    # Make a prediction
-    prediction = model.predict(image)[0]
-    label = np.argmax(prediction)
-    confidence = prediction[label]
-
-    # Return the prediction results as JSON
-    response = {
-        'label': str(label),
-        'confidence': str(confidence)
-    }
-    return jsonify(response)
-
+# Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
