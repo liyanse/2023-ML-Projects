091806c31f45753f6d3229c970fa627e43bee355
Lians Wanjiku <91662935+liyanse@users.noreply.github.com>
2023-03-01 19:17:13 +0300

    wed

--- Getting Started with Docker/main.py
+++ Getting Started with Docker/main.py
@@ -1,33 +1,28 @@
-#import libraries
 import pickle
 import numpy as np
 from PIL import Image
-from tensorflow.keras.preprocessing.image import img_to_array
-from flask import Flask, request, render_template, jsonify
 
-# Create an instance of Flask
-app = Flask(__name__)
+# Load the trained model from the pickle file
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+    
 
-# Load pretrained TF model
-model = pickle.load(open('model.pickle', 'rb'))
+# Open the image
+image = Image.open('image1.jpg')
 
-# Define the route for the prediction
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get the image passed in
-    image = request.files['image']
-    # Convert the image to an array
-    image_array = img_to_array(Image.open(image))
-    # Reshape the image according to model
-    image_array = image_array.reshape((1, image_array.shape[0], image_array.shape[1], image_array.shape[2]))
-    # Make the prediction
-    prediction = model.predict(image_array)
-    # Get the predicted class label
-    prediction_class = np.argmax(prediction)
-    # Return the results
-    return render_template('index.html', prediction_class=prediction_class,
-                           image=image)
+# Preprocess the image
+image = image.resize((224, 224))  # Resize the image to match the input size of the model
+image = np.array(image)  # Convert the image to a numpy array
+image = image / 255.0  # Normalize the pixel values
 
-# Run the app
-if __name__ == '__main__':
-    app.run()
+# Add a batch dimension to the image
+image = np.expand_dims(image, axis=0)
+
+# Use the loaded model to predict the class of the image
+prediction = model.predict(image)
+
+# Output the predicted class name
+class_names = ['class1', 'class2', 'class3']  # Replace with your own class names
+predicted_class_index = np.argmax(prediction)
+predicted_class_name = class_names[predicted_class_index]
+print(predicted_class_name)
