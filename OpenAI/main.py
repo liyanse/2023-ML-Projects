5078397809bff9a5baeb564dcf83631d705fc77a
Lians Wanjiku <91662935+liyanse@users.noreply.github.com>
2023-03-07 13:40:35 +0300

    tuesday

---
+++ OpenAI/main.py
@@ -0,0 +1,32 @@
+# Import necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the dataset
+df = pd.read_csv('CO2 Emissions_Canada.csv')
+
+# Split the dataset into features and target variable
+X = df.iloc[:, 4:13]  # Features
+y = df.iloc[:, 13]    # Target variable
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a linear regression model
+model = LinearRegression()
+
+# Fit the model on the training data
+model.fit(X_train, y_train)
+
+# Make predictions on the testing data
+y_pred = model.predict(X_test)
+
+# Evaluate the model using mean squared error and R-squared score
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Mean Squared Error:", mse)
+print("R-Squared Score:", r2)
