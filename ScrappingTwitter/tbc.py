c86f1f4bee3a34ba070c476b5256a5b82690a888
liyanse <lianswanjiku@icloud.com>
2023-04-06 18:26:07 +0300

    updates

--- ScrappingTwitter/tbc.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-import numpy as np 
-import snscrape.modules.twitter as sntwitter 
-import datetime 
-from tqdm.notebook import tqdm_notebook 
-import seaborn as sns 
-import matplotlib.pyplot as plt 
-sns.set_theme(style="whitegrid") 
