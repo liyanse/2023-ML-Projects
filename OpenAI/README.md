6bf7fb72fa8aca906fcabc962b4665e837171151
liyanse <lianswanjiku@icloud.com>
2023-04-06 18:24:53 +0300

    new commit

--- OpenAI/README.md
+++
@@ -1,4 +0,0 @@
-# flask-app-template
-Starting template for a python Flask application
-
-Check out out [YouTube Channel here](https://www.youtube.com/c/SkoloOnline)
---
+++ ScrappingTwitter/tbc.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+import numpy as np 
+import snscrape.modules.twitter as sntwitter 
+import datetime 
+from tqdm.notebook import tqdm_notebook 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+sns.set_theme(style="whitegrid") 
